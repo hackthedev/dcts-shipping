56220e5d8c9059842e70abe229c17444d236144c
Marcel <40896559+hackthedev@users.noreply.github.com>
2025-12-03 21:07:34 +0100

    .

--- public/js/core/Client.js
+++ public/js/core/Client.js
@@ -1,11 +1,25 @@
 function Client() {
-    return window?.chrome?.webview?.hostObjects?.dcts;
+    return TauriProxy;
 }
 
 function isLauncher() {
-    try {
-        return !!Client()
-    } catch {
-        return false;
-    }
+    return !!window.__TAURI__;
 }
+
+
+const TauriProxy = new Proxy({}, {
+    get(target, prop) {
+        return async (...args) => {
+            let params = {};
+            if (args.length === 1 && typeof args[0] === "object") {
+                params = args[0];
+            } else {
+                args.forEach((a, i) => {
+                    params["arg" + i] = a;
+                });
+            }
+
+            return await window.__TAURI__.invoke(prop.toString(), params);
+        };
+    }
+});

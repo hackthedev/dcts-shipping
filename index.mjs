7ba96d567355f391c0f4624454b42d173a8ce6d6
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-03-14 14:45:45 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -926,7 +926,6 @@ export async function startServer() {
     var port = process.env.PORT || serverconfig.serverinfo.port;
     server.listen(port, function () {
         Logger.info("Server is running on port " + port);
-        console.log("Ready.")
 
         if (serverconfig.serverinfo.setup == 0) {
             var adminToken = generateId(64);

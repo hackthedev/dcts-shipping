796ab5878e70196e77dd32ba56ffc1ec00acd0ba
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-03-15 17:50:24 +0100

    .

--- ptero/entrypoint.sh
+++ ptero/entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 bun --version
 
 if [ -f "package.json" ]; then
-  bun install --frozen-lockfile 2>/dev/null || bun install
+  bun install --ignore-scripts --frozen-lockfile 2>/dev/null || bun install --ignore-scripts
 fi
 
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))

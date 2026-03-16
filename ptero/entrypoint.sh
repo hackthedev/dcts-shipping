5ee825847b0fe80fab4e1d93d4da4082e0909149
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-03-16 11:13:58 +0100

    Update entrypoint.sh

--- ptero/entrypoint.sh
+++ ptero/entrypoint.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+set -e
+
 cd /home/container
 
-if [ -d ".git" ]; then
-  echo "Pulling latest updates..."
-  git pull
+REPO_URL="${GIT_REPO:-https://github.com/hackthedev/dcts-shipping.git}"
+REPO_BRANCH="${GIT_BRANCH:-beta}"
+
+if [ ! -d ".git" ]; then
+  echo "No git repo found, cloning ${REPO_BRANCH}..."
+  rm -rf /home/container/*
+  git clone --branch "${REPO_BRANCH}" --single-branch "${REPO_URL}" /home/container
+else
+  echo "Updating repository..."
+  git fetch origin "${REPO_BRANCH}"
+  git checkout "${REPO_BRANCH}" || git checkout -b "${REPO_BRANCH}" "origin/${REPO_BRANCH}"
+  git reset --hard "origin/${REPO_BRANCH}"
+  git clean -fd
 fi
 
 bun --version
 
 if [ -f "package.json" ]; then
-  bun install --ignore-scripts --frozen-lockfile 2>/dev/null || bun install --ignore-scripts
+  bun install --ignore-scripts --frozen-lockfile || bun install --ignore-scripts
 fi
 
 MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
@@ -17,5 +29,3 @@ MODIFIED_STARTUP=$(eval "echo \"$MODIFIED_STARTUP\"")
 
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 exec bash -lc "${MODIFIED_STARTUP}"
-
-exec ${MODIFIED_STARTUP}

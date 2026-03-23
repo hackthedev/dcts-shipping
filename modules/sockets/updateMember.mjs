828934155da52fd9a140c97f338cd629feba9488
Alessandro Stagni <alessandro.stagni11@proton.me>
2026-03-23 18:48:22 +1300

    Handle missing information provided to updateMember function.

--- modules/sockets/updateMember.mjs
+++ modules/sockets/updateMember.mjs
@@ -6,11 +6,11 @@ export default (io) => (socket) => {
 
     socket.on('updateMember', async function (member, response) {
         try{
-            serverconfig.servermembers[member?.id].icon = member.newSettings.icon;
-            serverconfig.servermembers[member?.id].banner = member.newSettings.banner;
-            serverconfig.servermembers[member?.id].aboutme = member.newSettings.aboutme;
-            serverconfig.servermembers[member?.id].username = member.newSettings.username;
-            serverconfig.servermembers[member?.id].status = member.newSettings.status;
+            if(member?.newSettings?.icon !== undefined) serverconfig.servermembers[member?.id].icon = member.newSettings.icon;
+            if(member?.newSettings?.banner !== undefined) serverconfig.servermembers[member?.id].banner = member.newSettings.banner;
+            if(member?.newSettings?.aboutme !== undefined) serverconfig.servermembers[member?.id].aboutme = member.newSettings.aboutme;
+            if(member?.newSettings?.username !== undefined) serverconfig.servermembers[member?.id].username = member.newSettings.username;
+            if(member?.newSettings?.status !== undefined) serverconfig.servermembers[member?.id].status = member.newSettings.status;
             response({updatedMember: serverconfig.servermembers[member?.id]});
         }
         catch (exception){

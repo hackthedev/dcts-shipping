3eb002014adc9b0d20d20c620cbd4b4ed51beae1
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-03-28 06:08:09 +0100

    Test

--- ptero/Livekit/entrypoint.sh
+++ ptero/Livekit/entrypoint.sh
@@ -3,27 +3,42 @@ set -e
 
 cd /home/container
 
-REPO_URL="${GIT_REPO:-https://github.com/hackthedev/dcts-shipping.git}"
-REPO_BRANCH="${GIT_BRANCH:-beta}"
+export LIVEKIT_YAML_PATH="${LIVEKIT_YAML_PATH:-livekit.yaml}"
 
-if [ ! -d ".git" ]; then
-  echo "Force cloning repo..."
-  rm -rf /home/container/* /home/container/.[!.]* 2>/dev/null || true
-  git clone --branch "${GIT_BRANCH:-beta}" "${GIT_REPO:-https://github.com/hackthedev/dcts-shipping.git}" /home/container
+if [ ! -f "livekit-server" ]; then
+    echo "ERROR: livekit-server binary not found! Please reinstall the server or check the installation script."
+    exit 1
+fi
+
+if ! grep -q "^keys:" "${LIVEKIT_YAML_PATH}" 2>/dev/null; then
+    echo "Generating LiveKit keys..."
+
+    OUTPUT=$(./livekit-server generate-keys)
+
+    API_KEY=$(echo "$OUTPUT" | awk '/API Key:/ {print $3}')
+    API_SECRET=$(echo "$OUTPUT" | awk '/API Secret:/ {print $3}')
+
+    if [ -z "$API_KEY" ] || [ -z "$API_SECRET" ]; then
+        echo "Key generation failed!"
+        exit 1
+    fi
+
+    touch "${LIVEKIT_YAML_PATH}"
+
+    /usr/local/bin/yq -i '
+      .keys = {} |
+      .keys["'"$API_KEY"'"] = "'"$API_SECRET"'"
+    ' "${LIVEKIT_YAML_PATH}"
+
+    echo "Keys written. Your API Key is: ${API_KEY}"
 else
-  git fetch origin "${GIT_BRANCH:-beta}"
-  git reset --hard "origin/${GIT_BRANCH:-beta}"
-  git clean -fd
+    echo "Keys already exist."
 fi
 
-bun --version
-
-if [ -f "package.json" ]; then
-  bun install --ignore-scripts --frozen-lockfile || bun install --ignore-scripts
+# Replace any variables if we need to
+if [ -n "$SERVER_PORT" ]; then
+    /usr/local/bin/yq -i '.port = '"$SERVER_PORT" "${LIVEKIT_YAML_PATH}"
 fi
 
-MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-MODIFIED_STARTUP=$(eval "echo \"$MODIFIED_STARTUP\"")
-
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-exec bash -lc "${MODIFIED_STARTUP}"
+# Start livekit
+exec ./livekit-server --config "${LIVEKIT_YAML_PATH}"

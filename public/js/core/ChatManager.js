1681ba7e2e73c8f8fedfeb355d6e19f367958633
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-08-20 20:00:20 +0200

    proxy bug fix

--- public/js/core/ChatManager.js
+++ public/js/core/ChatManager.js
@@ -994,13 +994,13 @@ class ChatManager {
         if (!url) return null;
         if (url.startsWith(window.location.origin)) return url;
         if (url.startsWith(window.location.origin)) return encodeURIComponent(url);
-        if (url.startsWith("data:")) return encodeURIComponent(url);
-        if (url.startsWith("/uploads")) return encodeURIComponent(url);
-        if (url.startsWith("/upload")) return encodeURIComponent(url);
-        if (url.startsWith("/emojis")) return encodeURIComponent(url);
-        if (url.startsWith("/uploads") || url.startsWith("/upload")) return encodeURIComponent(url);
-        if (url.startsWith("/img") || url.startsWith("img/")) return encodeURIComponent(url);
-        if (url.startsWith("/emojis")) return encodeURIComponent(url);
+        if (url.startsWith("data:")) return (url);
+        if (url.startsWith("/uploads")) return (url);
+        if (url.startsWith("/upload")) return (url);
+        if (url.startsWith("/emojis")) return (url);
+        if (url.startsWith("/uploads") || url.startsWith("/upload")) return (url);
+        if (url.startsWith("/img") || url.startsWith("img/")) return (url);
+        if (url.startsWith("/emojis")) return (url);
         return `/proxy?url=${encodeURIComponent(url)}`
     }
 

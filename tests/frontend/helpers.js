b8601edfc03cec671b45f8fef20f57720ae05a65
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-08-20 21:26:15 +0200

    some improvement

---
+++ tests/frontend/helpers.js
@@ -0,0 +1,33 @@
+export function getMockMember(){
+    return {
+        id: 123456789102,
+        name: "Test",
+        icon: null,
+        banner: null,
+        card: null,
+        roles: [
+            {
+                id: 1111,
+                name: "Administrator",
+                icon: null,
+                color: "transparent",
+                deletable: 0,
+                sortId: 4,
+                displaySeperate: 1,
+                hasRole: 1,
+                background: "linear-gradient(90deg, rgb(254, 22, 22), rgb(180, 179, 255), rgb(6, 254, 192)) text",
+                backgroundClip: "text",
+                members: [
+                    "122302074345",
+                    "131176644302",
+                    "179079590204",
+                    "150796613356",
+                    "150029071694",
+                    "162978572482",
+                    "372043115152015362",
+                    "148159172677"
+                ]
+            }
+        ]
+    };
+}
